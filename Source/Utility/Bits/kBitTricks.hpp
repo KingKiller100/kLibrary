d9e80703a88af0de43637402aa1759d347b6ce91
KingKayDee <kwekudeheer@ymail.com>
2021-01-08 03:37:00 +0000

    Added exceptions if index given is out of range of object

--- Source/Utility/Bits/kBitTricks.hpp
+++ Source/Utility/Bits/kBitTricks.hpp
@@ -8,24 +8,32 @@ namespace klib::bits
 	template<class Integer, class = std::enable_if_t<type_trait::Is_Integer_V<Integer>>>
 	constexpr void SetBit(Integer& num, size_t index)
 	{
+		if (index > std::numeric_limits<Integer>::digits)
+			std::_Xout_of_range("Index greater than bits in this type");
 		num |= 1ull << index;
 	}
 	
 	template<class Integer, class = std::enable_if_t<type_trait::Is_Integer_V<Integer>>>
 	constexpr void ClearBit(Integer& num, size_t index)
 	{
+		if (index > std::numeric_limits<Integer>::digits)
+			std::_Xout_of_range("Index greater than bits in this type");
 		num &= ~(1ull << index);
 	}
 	
 	template<class Integer, class = std::enable_if_t<type_trait::Is_Integer_V<Integer>>>
 	constexpr void ToggleBit(Integer& num, size_t index)
 	{
+		if (index > std::numeric_limits<Integer>::digits)
+			std::_Xout_of_range("Index greater than bits in this type");
 		num ^= 1ull << index;
 	}
 	
 	template<class Integer, class = std::enable_if_t<type_trait::Is_Integer_V<Integer>>>
 	USE_RESULT constexpr bool CheckBit(Integer num, size_t index)
 	{
+		if (index > std::numeric_limits<Integer>::digits)
+			std::_Xout_of_range("Index greater than bits in this type");
 		return (num >> index) & 1;
 	}
 	
