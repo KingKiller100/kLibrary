98ed1dccae9a06c866df283a2a54c364353b0592
KingKayDee <kwekudeheer@ymail.com>
2021-01-21 04:28:11 +0000

    Making sure k_s_n works on platform and str ops + adding float_t [C]StrTo

--- Source/Platform/kPlatform.hpp
+++ Source/Platform/kPlatform.hpp
@@ -4,20 +4,28 @@
 
 #include "../Utility/Enum/kEnum.hpp"
 
-namespace klib::kPlatform
+namespace klib
 {
-	ENUM_CLASS(PlatformOS, std::uint8_t,
-		UNKNOWN = 0,
-		WINDOWS_32 = BIT_SHIFT(0),
-		WINDOWS_64 = BIT_SHIFT(1),
-		WINDOWS = WINDOWS_32 | WINDOWS_64,
-		IOS_SIM = BIT_SHIFT(2),
-		IOS = BIT_SHIFT(3),
-		MAC = BIT_SHIFT(4),
-		APPLE = IOS_SIM | IOS | MAC,
-		ANDROID = BIT_SHIFT(5),
-		LINUX = ANDROID + 1
+	namespace kPlatform
+	{
+		ENUM_CLASS(PlatformOS, std::uint8_t,
+			UNKNOWN = 0,
+			WINDOWS_32 = BIT_SHIFT(0),
+			WINDOWS_64 = BIT_SHIFT(1),
+			WINDOWS = WINDOWS_32 | WINDOWS_64,
+			IOS_SIM = BIT_SHIFT(2),
+			IOS = BIT_SHIFT(3),
+			MAC = BIT_SHIFT(4),
+			APPLE = IOS_SIM | IOS | MAC,
+			ANDROID = BIT_SHIFT(5),
+			LINUX = ANDROID + 1
 		);
 
-	USE_RESULT PlatformOS GetPlatform() noexcept;
+		USE_RESULT PlatformOS GetPlatform() noexcept;
+	}
+ 
+#ifdef KLIB_SHORT_NAMESPACE
+	using namespace kPlatform;
+#endif
+	
 }
--- Source/Utility/String/Tricks/kStringTo.hpp
+++ Source/Utility/String/Tricks/kStringTo.hpp
@@ -1,6 +1,7 @@
 ﻿#pragma once
 
 #include "kStringSize.hpp"
+#include "kStringFind.hpp"
 #include "kStringDigitChar.hpp"
 #include "kStringRemove.hpp"
 
@@ -13,21 +14,19 @@
 
 namespace klib::kString
 {
-	template<class Integer_t, class Char_t, class = std::enable_if_t<
+	template<class Arithmetic_t, class Char_t, class = std::enable_if_t<
 		type_trait::Is_Char_V<Char_t>
+		&& std::is_arithmetic_v<Arithmetic_t>
 		>>
-		USE_RESULT constexpr Integer_t CStrTo(const Char_t* const str, const Char_t* const end = nullptr)
+		USE_RESULT constexpr Arithmetic_t CStrTo(const Char_t* const str, const Char_t* const end = nullptr)
 	{
-		static_assert(std::is_integral_v<Integer_t>, __FUNCTION__ " can only be used with integer types "
-			"(char, int, long, unsigned short, etc..");
-
 		const auto CrashFunc = [](const std::string& errMsg)
 		{
 			throw kDebug::StringError(errMsg + '\n');
 		};
 		const auto MaxDigitsFunc = []()
 		{
-			auto maxNum = std::numeric_limits<Integer_t>::max();
+			auto maxNum = std::numeric_limits<Arithmetic_t>::max();
 			size_t count = 0;
 			do {
 				++count;
@@ -38,7 +37,7 @@ namespace klib::kString
 
 		if (str == nullptr
 			|| str[0] == type_trait::g_NullTerminator<Char_t>)
-			return static_cast<Integer_t>(0);
+			return static_cast<Arithmetic_t>(0);
 
 		std::function<bool(const Char_t* const s, size_t pos, const Char_t* const)> endCond;
 		if (end == nullptr)
@@ -56,63 +55,94 @@ namespace klib::kString
 			};
 		}
 
-		const auto isNeg = std::is_signed_v<Integer_t>
+		const auto isNeg = std::is_signed_v<Arithmetic_t>
 			&& str[0] == Char_t('-');
 
-		Integer_t result = 0;
+		Arithmetic_t result = 0;
 		size_t currentPos = isNeg ? 1 : 0;
 		size_t size = isNeg ? GetSize(str) - 1 : GetSize(str);
 		auto magnitude = static_cast<size_t>(std::pow(10, size - 1));
 
 		if (size > MaxDigitsFunc())
 		{
-			const std::string type = typeid(Integer_t).name();
+			const std::string type = typeid(Arithmetic_t).name();
 			const auto msg = __FUNCTION__ "'s string contains more digits than largest number of type: "
 				+ type;
 			CrashFunc(msg);
 		}
 
-		while (endCond(str, currentPos, end))
+		if constexpr (std::is_integral_v<Arithmetic_t>)
 		{
-			if (!IsDigit(str[currentPos]))
+			while (endCond(str, currentPos, end))
 			{
-				const auto input = ToWriter(Convert<char>(str));
-				CrashFunc(__FUNCTION__ "'s string must only contain digits. string: " + input);
+				if (!IsDigit(str[currentPos]))
+				{
+					const auto input = ToWriter(Convert<char>(str));
+					CrashFunc(__FUNCTION__ "'s string must only contain digits. string: " + input);
+				}
+
+				const auto digit = static_cast<std::make_unsigned_t<Arithmetic_t>>(str[currentPos] - Char_t('0'));
+				const auto asInt = digit * magnitude;
+
+				result += static_cast<Arithmetic_t>(asInt);
+				magnitude /= 10;
+				++currentPos;
 			}
 
-			const auto digit = static_cast<std::make_unsigned_t<Integer_t>>(str[currentPos] - Char_t('0'));
-			const auto asInt = digit * magnitude;
+			if (isNeg)
+				result *= -1;
 
-			result += static_cast<Integer_t>(asInt);
-			magnitude /= 10;
-			++currentPos;
+			return result;
 		}
+		else // Assume it is a floating point
+		{
+			auto decimalPos = Find(str, Char_t('.'));
 
-		if (isNeg)
-			result *= -1;
+			if (decimalPos == type_trait::g_NullTerminator<Char_t>)
+			{
+				result = static_cast<Arithmetic_t>(CStrTo<long long>(str, end));
+				return result;
+			}
 
-		return result;
+			auto integerEnd = str + decimalPos;
+			result = static_cast<Arithmetic_t>(CStrTo<long long>(str, integerEnd));
+
+			++decimalPos;
+			
+			auto remaining = end == nullptr ? GetSize(str) - decimalPos : (end - str) - decimalPos;
+
+			if (remaining > std::numeric_limits<Arithmetic_t>::max_digits10)
+				remaining = std::numeric_limits<Arithmetic_t>::max_digits10;
+
+			auto decimalStart = str + decimalPos;
+			
+			auto decimals = static_cast<Arithmetic_t>(CStrTo<long long>(decimalStart));
+			
+			decimals /= std::pow(10, remaining);
+			result += decimals;
+			return result;
+		}
 	}
 
-	template<class Integer_t, class Char_t, class = std::enable_if_t<
+	template<class Arithmetic_t, class Char_t, class = std::enable_if_t<
 		type_trait::Is_Char_V<Char_t>
 		>>
-		USE_RESULT constexpr Integer_t CStrTo(const Char_t* const cstr, Integer_t defaultValue, const Char_t* const end = nullptr)
+		USE_RESULT constexpr Arithmetic_t CStrTo(const Char_t* const cstr, Arithmetic_t defaultValue, const Char_t* const end = nullptr)
 	{
-		const auto val = CStrTo<Integer_t>(cstr, end);
+		const auto val = CStrTo<Arithmetic_t>(cstr, end);
 		if (val == 0)
 			return defaultValue;
 		return val;
 	}
 
-	template<class Integer_t, class Char_t, class = std::enable_if_t<
+	template<class Arithmetic_t, class Char_t, class = std::enable_if_t<
 		type_trait::Is_Char_V<Char_t>
 		>>
-		USE_RESULT constexpr Integer_t TryCStrTo(const Char_t* const cstr, Integer_t defaultValue, const Char_t* const end = nullptr)
+		USE_RESULT constexpr Arithmetic_t TryCStrTo(const Char_t* const cstr, Arithmetic_t defaultValue, const Char_t* const end = nullptr)
 	{
 		try
 		{
-			return CStrTo<Integer_t>(cstr, end);
+			return CStrTo<Arithmetic_t>(cstr, end);
 		}
 		catch (...)
 		{
@@ -121,11 +151,11 @@ namespace klib::kString
 	}
 
 	// Converts strings containing text for an integer value into the integer type with that value 
-	template<class Integer_t, typename StringT
+	template<class Arithmetic_t, typename StringT
 		, typename = std::enable_if_t<
 		type_trait::Is_Specialization_V<StringT, std::basic_string>
 		>>
-		USE_RESULT constexpr Integer_t StrTo(StringT string, size_t count = type_trait::g_NoPos<StringT>)
+		USE_RESULT constexpr Arithmetic_t StrTo(StringT string, size_t count = type_trait::g_NoPos<StringT>)
 	{
 		using CharType = typename StringT::value_type;
 
@@ -139,7 +169,7 @@ namespace klib::kString
 		Remove(string, CharType('\''));
 
 		if (string.empty())
-			return static_cast<Integer_t>(0);
+			return static_cast<Arithmetic_t>(0);
 
 		if (Contains(string, CharType('.')))
 			CrashFunc("string must contain only one integer number");
@@ -150,20 +180,20 @@ namespace klib::kString
 			? nullptr
 			: data + count;
 
-		const auto result = CStrTo<Integer_t>(data, end);
+		const auto result = CStrTo<Arithmetic_t>(data, end);
 
 		return result;
 	}
 
 	// Converts strings containing text for an integer value into the integer type with that value.
 	// But if zero results are unwanted, a default value can be returned instead
-	template<class Integer_t, typename StringT
+	template<class Arithmetic_t, typename StringT
 		, typename = std::enable_if_t<
 		type_trait::Is_Specialization_V<StringT, std::basic_string>
 		>>
-		USE_RESULT constexpr Integer_t StrTo(StringT string, Integer_t defaultValue, size_t count = type_trait::g_NoPos<StringT>)
+		USE_RESULT constexpr Arithmetic_t StrTo(StringT string, Arithmetic_t defaultValue, size_t count = type_trait::g_NoPos<StringT>)
 	{
-		const auto val = StrTo<Integer_t>(string, count);
+		const auto val = StrTo<Arithmetic_t>(string, count);
 		if (val == 0)
 			return defaultValue;
 		return val;
@@ -171,15 +201,15 @@ namespace klib::kString
 
 	// Converts strings containing text for an integer value into the integer type with that value.
 	// But bad string inputs are likely, ignore the exception and return a default value
-	template<class Integer_t, typename StringT
+	template<class Arithmetic_t, typename StringT
 		, typename = std::enable_if_t<
 		type_trait::Is_Specialization_V<StringT, std::basic_string>
 		>>
-		USE_RESULT constexpr Integer_t TryStrTo(StringT string, Integer_t defaultValue, size_t count = type_trait::g_NoPos<StringT>)
+		USE_RESULT constexpr Arithmetic_t TryStrTo(StringT string, Arithmetic_t defaultValue, size_t count = type_trait::g_NoPos<StringT>)
 	{
 		try
 		{
-			return StrTo<Integer_t>(string, count);
+			return StrTo<Arithmetic_t>(string, count);
 		}
 		catch (...)
 		{
