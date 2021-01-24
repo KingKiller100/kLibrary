ab3837fa65c4cec1cacafb01be600b53c9e3c72e
KingKayDee <kwekudeheer@ymail.com>
2021-01-24 03:34:18 +0000

    Added k_s_n for short namespace

--- Source/Utility/Bits/kBitTricks.hpp
+++ Source/Utility/Bits/kBitTricks.hpp
@@ -3,44 +3,51 @@
 #include "../../HelperMacros.hpp"
 #include "../../TypeTraits/IntegerTraits.hpp"
 
-namespace klib::kBits
+namespace klib
 {
-	template<class Integer, class = std::enable_if_t<type_trait::Is_Integer_V<Integer>>>
-	constexpr void SetBit(Integer& num, size_t index)
+	namespace kBits
 	{
-		if (index > std::numeric_limits<Integer>::digits)
-			std::_Xout_of_range("Index greater than bits in this type");
-		num |= 1ull << index;
+		template<class Integer, class = std::enable_if_t<type_trait::Is_Integer_V<Integer>>>
+		constexpr void SetBit(Integer& num, size_t index)
+		{
+			if (index > std::numeric_limits<Integer>::digits - 1)
+				std::_Xout_of_range("Index greater than bits in this type");
+			num |= 1ull << index;
+		}
+
+		template<class Integer, class = std::enable_if_t<type_trait::Is_Integer_V<Integer>>>
+		constexpr void ClearBit(Integer& num, size_t index)
+		{
+			if (index > std::numeric_limits<Integer>::digits - 1)
+				std::_Xout_of_range("Index greater than bits in this type");
+			num &= ~(1ull << index);
+		}
+
+		template<class Integer, class = std::enable_if_t<type_trait::Is_Integer_V<Integer>>>
+		constexpr void ToggleBit(Integer& num, size_t index)
+		{
+			if (index > std::numeric_limits<Integer>::digits - 1)
+				std::_Xout_of_range("Index greater than bits in this type");
+			num ^= 1ull << index;
+		}
+
+		template<class Integer, class = std::enable_if_t<type_trait::Is_Integer_V<Integer>>>
+		USE_RESULT constexpr bool CheckBit(Integer num, size_t index)
+		{
+			if (index > std::numeric_limits<Integer>::digits - 1)
+				std::_Xout_of_range("Index greater than bits in this type");
+			return (num >> index) & 1;
+		}
+
+		template<class Integer, class = std::enable_if_t<type_trait::Is_Integer_V<Integer>>>
+		constexpr void SetBit(Integer& num, size_t index, bool value)
+		{
+			const auto bit = value ? 0b1ull : 0b0ull;
+			num = static_cast<Integer>(num & ~(1ull << index) | bit << index);
+		}
 	}
-	
-	template<class Integer, class = std::enable_if_t<type_trait::Is_Integer_V<Integer>>>
-	constexpr void ClearBit(Integer& num, size_t index)
-	{
-		if (index > std::numeric_limits<Integer>::digits)
-			std::_Xout_of_range("Index greater than bits in this type");
-		num &= ~(1ull << index);
-	}
-	
-	template<class Integer, class = std::enable_if_t<type_trait::Is_Integer_V<Integer>>>
-	constexpr void ToggleBit(Integer& num, size_t index)
-	{
-		if (index > std::numeric_limits<Integer>::digits)
-			std::_Xout_of_range("Index greater than bits in this type");
-		num ^= 1ull << index;
-	}
-	
-	template<class Integer, class = std::enable_if_t<type_trait::Is_Integer_V<Integer>>>
-	USE_RESULT constexpr bool CheckBit(Integer num, size_t index)
-	{
-		if (index > std::numeric_limits<Integer>::digits)
-			std::_Xout_of_range("Index greater than bits in this type");
-		return (num >> index) & 1;
-	}
-	
-	template<class Integer, class = std::enable_if_t<type_trait::Is_Integer_V<Integer>>>
-	constexpr void SetBit(Integer& num, size_t index, bool value)
-	{
-		const auto bit = value ? 0b1ull : 0b0ull;
-		num = static_cast<Integer>(num & ~(1ull << index) | bit << index);
-	}	
+
+#ifdef KLIB_SHORT_NAMESPACE
+	using namespace kBits;
+#endif
 }
