ecd4786b415c5dc3af51b67435a51ef273d1ee50
Karel de Heer <karel.deheer@inseinc.com>
2021-03-17 17:26:06 +0000

    Added a IsRuntimeDebug() call to allow user to check runtime config during runtime

--- Source/Utility/Debug/kDebugger.hpp
+++ Source/Utility/Debug/kDebugger.hpp
@@ -5,6 +5,17 @@ namespace klib
 {
 	namespace kDebug
 	{
+		constexpr bool IsRuntimeDebug() noexcept
+		{
+			return 
+#if !defined(NDEBUG) || defined(KLIB_DEBUG)
+			true;
+#else
+
+			false;
+#endif
+		}
+		
 		template<class CharT>
 		void IsDebuggerAttached(const CharT* filename, std::chrono::milliseconds = std::chrono::milliseconds(500)) noexcept;
 		void BreakPoint() noexcept;
