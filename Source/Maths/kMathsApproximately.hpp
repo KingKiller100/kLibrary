d796544f0ba4fee1e3c46f024b4b092adc61aed9
King Kay Dee <kwekudeheer@ymail.com>
2021-09-15 00:16:48 +0100

    Changed impl for Approximately to take into count unsigned values lb loop to max values

--- Source/Maths/kMathsApproximately.hpp
+++ Source/Maths/kMathsApproximately.hpp
@@ -7,10 +7,30 @@ namespace kmaths
 	template<class T1, typename T2, typename T3>
 	USE_RESULT constexpr bool Approximately(T1 value, T2 target, T3 allowance) noexcept
 	{
-		const auto lb = static_cast<T1>(target) - allowance;
 		const auto ub = static_cast<T1>(target) + allowance;
 
-		return (value >= lb) && (value <= ub);
+		if constexpr (std::is_unsigned_v<T1>)
+		{
+			if (target == constants::Zero<T1>)
+			{
+				return value <= ub;
+			}
+			else
+			{
+				const auto lb = static_cast<T1>(target) - allowance;
+				if (lb > ub) // Buffer underflow occurred
+				{
+					return value <= ub;
+				}
+				return (value >= lb) && (value <= ub);
+			}
+		}
+		else
+		{
+			const auto lb = static_cast<T1>(target) - allowance;
+
+			return (value >= lb) && (value <= ub);
+		}
 	}
 
 	template<class T1, typename T2>
