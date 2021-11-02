8e1713861d299d1130e89db16ce01648476c9355
King Kay Dee <kwekudeheer@ymail.com>
2021-11-02 23:46:17 +0000

    Resharper formatted doc

--- Source/Utility/String/kStringTricks.hpp
+++ Source/Utility/String/kStringTricks.hpp
@@ -21,20 +21,20 @@ namespace klib
 {
 	namespace kString
 	{
-		ENUM_CLASS(PreserveToken, std::uint8_t, YES, NO);
-		ENUM_CLASS(PreserveEmpty, std::uint8_t, YES, NO);
+		ENUM_CLASS( PreserveToken, std::uint8_t, YES, NO );
+		ENUM_CLASS( PreserveEmpty, std::uint8_t, YES, NO );
 
-		template<class CharT = char, class = std::enable_if_t<
-			type_trait::Is_Char_V<CharT>
-			>>
-		USE_RESULT constexpr StringWriter<CharT> Replace(const CharT* str, CharT oldChar, CharT newChar) noexcept
+		template <class CharT = char, class = std::enable_if_t<
+			          type_trait::Is_Char_V<CharT>
+		          >>
+		USE_RESULT constexpr StringWriter<CharT> Replace( const CharT* str, CharT oldChar, CharT newChar ) noexcept
 		{
 			using StrW = StringWriter<CharT>;
 
-			auto text = StrW(str);
+			auto text = StrW( str );
 
 #if MSVC_PLATFORM_TOOLSET >= 141
-			std::replace(text.begin(), text.end(), oldChar, newChar);
+			std::replace( text.begin(), text.end(), oldChar, newChar );
 #else
 			auto oldCharPos = Find_First_Of(str, oldChar);
 
@@ -52,82 +52,92 @@ namespace klib
 			return text;
 		}
 
-		template<class CharT = char, class = std::enable_if_t<
-			type_trait::Is_Char_V<CharT>
-			>>
-		USE_RESULT constexpr StringWriter<CharT> Replace(const CharT* str, const CharT* oldStr, const CharT* newStr) noexcept
+		template <class CharT = char, class = std::enable_if_t<
+			          type_trait::Is_Char_V<CharT>
+		          >>
+		USE_RESULT constexpr StringWriter<CharT> Replace(
+			const CharT* str, const CharT* oldStr, const CharT* newStr
+		) noexcept
 		{
 			using StrW = StringWriter<CharT>;
 
-			auto text = StrW(str);
-			
-			auto oldStrPos = Find(str, oldStr);
+			auto text = StrW( str );
 
-			if (oldStrPos == StrW::npos)
+			auto oldStrPos = Find( str, oldStr );
+
+			if ( oldStrPos == StrW::npos )
 				return text;
 
-			if (str[0] == oldStr)
+			if ( str[0] == oldStr )
 				text[0] = oldStr;
 
-			do {
+			do
+			{
 				text[oldStrPos] = newStr;
-				oldStrPos = Find_First_Of(text.data(), oldStr);
-			} while (oldStrPos != StrW::npos);
+				oldStrPos = Find_First_Of( text.data(), oldStr );
+			}
+			while ( oldStrPos != StrW::npos );
 
 			return text;
 		}
 
-		template<class StringT, typename = std::enable_if_t<
-			type_trait::Is_String_V<StringT>
-		>>
-		USE_RESULT constexpr StringWriter<typename StringT::value_type> Replace(const StringT& str, const StringT& oldChar, const StringT& newChar) noexcept
+		template <class StringT, typename = std::enable_if_t<
+			          type_trait::Is_String_V<StringT>
+		          >>
+		USE_RESULT constexpr StringWriter<typename StringT::value_type> Replace(
+			const StringT& str, const StringT& oldChar, const StringT& newChar
+		) noexcept
 		{
-			return Replace<typename StringT::value_type>(str.data(), oldChar.data(), newChar.data());
+			return Replace<typename StringT::value_type>( str.data(), oldChar.data(), newChar.data() );
 		}
 
-		template<class StringT, typename = std::enable_if_t<
-			type_trait::Is_String_V<StringT>
-		>>
-		USE_RESULT constexpr StringWriter<typename StringT::value_type> Replace(const StringT& str, typename StringT::value_type oldChar, typename StringT::value_type newChar) noexcept
+		template <class StringT, typename = std::enable_if_t<
+			          type_trait::Is_String_V<StringT>
+		          >>
+		USE_RESULT constexpr StringWriter<typename StringT::value_type> Replace(
+			const StringT& str, typename StringT::value_type oldChar, typename StringT::value_type newChar
+		) noexcept
 		{
-			return Replace<typename StringT::value_type>(str.data(), oldChar, newChar);
+			return Replace<typename StringT::value_type>( str.data(), oldChar, newChar );
 		}
 
-		template<class CharT = char>
-		USE_RESULT constexpr std::vector<StringWriter<CharT>> Split(const StringWriter<CharT>& str, const CharT* tokens,
-			const PreserveToken preserveToken = PreserveToken::NO, PreserveEmpty preserveEmpty = PreserveEmpty::NO)
+		template <class CharT = char>
+		USE_RESULT constexpr std::vector<StringWriter<CharT>> Split(
+			const StringWriter<CharT>& str, const CharT* tokens,
+			const PreserveToken preserveToken = PreserveToken::NO, PreserveEmpty preserveEmpty = PreserveEmpty::NO
+		)
 		{
 			using StrW = StringWriter<CharT>;
 
-			const auto keepToken = preserveToken.Compare(PreserveToken::YES);
-			const auto keepEmpty = preserveEmpty.Compare(PreserveEmpty::YES);
+			const auto keepToken = preserveToken.Compare( PreserveToken::YES );
+			const auto keepEmpty = preserveEmpty.Compare( PreserveEmpty::YES );
 
 			std::vector<StrW> lines;
 
 			size_t prevPos = 0;
 			size_t tokenPos = 0;
-			while (tokenPos != StrW::npos)
+			while ( tokenPos != StrW::npos )
 			{
-				tokenPos = str.find_first_of(tokens, prevPos);
-				const auto line = str.substr(prevPos, (keepToken ? tokenPos + 1 : tokenPos) - prevPos);
-				if (!line.empty() || keepEmpty)
-					lines.emplace_back(line);
+				tokenPos = str.find_first_of( tokens, prevPos );
+				const auto line = str.substr( prevPos, ( keepToken ? tokenPos + 1 : tokenPos ) - prevPos );
+				if ( !line.empty() || keepEmpty )
+					lines.emplace_back( line );
 				prevPos = tokenPos + 1;
 			}
 
 			return lines;
 		}
 
-		template<class CharT = char>
-		USE_RESULT constexpr std::vector<StringWriter<CharT>> Split(const StringWriter<CharT>& str, const StringReader<CharT>& tokens,
-			const PreserveToken preserveToken = PreserveToken::NO, PreserveEmpty preserveEmpty = PreserveEmpty::NO)
+		template <class CharT = char>
+		USE_RESULT constexpr std::vector<StringWriter<CharT>> Split(
+			const StringWriter<CharT>& str, const StringReader<CharT>& tokens,
+			const PreserveToken preserveToken = PreserveToken::NO, PreserveEmpty preserveEmpty = PreserveEmpty::NO
+		)
 		{
-			return Split(str, tokens.data(), preserveToken, preserveEmpty);
+			return Split( str, tokens.data(), preserveToken, preserveEmpty );
 		}
-
 	}
 #ifdef KLIB_SHORT_NAMESPACE
 	using namespace kString;
 #endif
 }
-
