f1800828ac292384e40da171b56cf8925b9d7d4c
King Kay Dee <kwekudeheer@ymail.com>
2021-11-10 00:35:45 +0000

    Formatting files

--- Source/Utility/FileSystem/kPathString.hpp
+++ Source/Utility/FileSystem/kPathString.hpp
@@ -7,23 +7,23 @@ namespace klib
 	namespace kFileSystem
 	{
 		// Dynamic string buffer
-		template<typename CharT, typename AllocT = std::allocator<CharT>>
+		template <typename CharT, typename AllocT = std::allocator<CharT>>
 		using DOSPathString = std::basic_string<CharT, DOSPathTraits<CharT>, AllocT>;
 
-		template<typename CharT, typename AllocT = std::allocator<CharT>>
+		template <typename CharT, typename AllocT = std::allocator<CharT>>
 		using UnixPathString = std::basic_string<CharT, UnixPathTraits<CharT>, AllocT>;
 
-		template<typename CharT, typename AllocT = std::allocator<CharT>>
+		template <typename CharT, typename AllocT = std::allocator<CharT>>
 		using PathString = std::basic_string<CharT, PathTraits<CharT>, AllocT>;
-		
+
 		// Visual string buffer
-		template<typename CharT>
+		template <typename CharT>
 		using DOSPathStringView = std::basic_string_view<CharT, DOSPathTraits<CharT>>;
 
-		template<typename CharT>
+		template <typename CharT>
 		using UnixPathStringView = std::basic_string_view<CharT, UnixPathTraits<CharT>>;
 
-		template<typename CharT>
+		template <typename CharT>
 		using PathStringView = std::basic_string_view<CharT, PathTraits<CharT>>;
 	}
 
--- Source/Utility/FileSystem/kPathTraits.hpp
+++ Source/Utility/FileSystem/kPathTraits.hpp
@@ -7,7 +7,7 @@ namespace klib
 {
 	namespace kFileSystem
 	{
-		template<typename CharT>
+		template <typename CharT>
 		struct UnixPathTraits : public std::char_traits<CharT>
 		{
 		public:
@@ -17,11 +17,15 @@ namespace klib
 			using Char_t = CharT;
 			using Base_t = std::char_traits<Char_t>;
 
-			USE_RESULT static constexpr const char* find(const Char_t* haystack, size_t count,
-				const Char_t& needle) noexcept
+			USE_RESULT static constexpr const char* find(
+				const Char_t* haystack, size_t count,
+				const Char_t& needle
+			) noexcept
 			{
-				for (; 0 < count; --count, ++haystack) {
-					if (ResolveCharacter(*haystack) == ResolveCharacter(needle)) {
+				for ( ; 0 < count; --count, ++haystack )
+				{
+					if ( ResolveCharacter( *haystack ) == ResolveCharacter( needle ) )
+					{
 						return haystack;
 					}
 				}
@@ -29,14 +33,16 @@ namespace klib
 				return nullptr;
 			}
 
-			USE_RESULT static constexpr int compare(const Char_t* strA,
-				const Char_t* strB, size_t count) noexcept
+			USE_RESULT static constexpr int compare(
+				const Char_t* strA,
+				const Char_t* strB, size_t count
+			) noexcept
 			{
-				for (; 0 < count; --count, ++strA, ++strB)
+				for ( ; 0 < count; --count, ++strA, ++strB )
 				{
-					const auto letterA = ResolveCharacter(*strA);
-					const auto letterB = ResolveCharacter(*strB);
-					if (letterA != letterB)
+					const auto letterA = ResolveCharacter( *strA );
+					const auto letterB = ResolveCharacter( *strB );
+					if ( letterA != letterB )
 					{
 						return letterA < letterB ? -1 : 1;
 					}
@@ -44,28 +50,27 @@ namespace klib
 				return 0;
 			}
 
-			USE_RESULT static constexpr bool eq(const Char_t& lhs, const Char_t& rhs) noexcept
+			USE_RESULT static constexpr bool eq( const Char_t& lhs, const Char_t& rhs ) noexcept
 			{
-				return ResolveCharacter(lhs) == ResolveCharacter(rhs);
+				return ResolveCharacter( lhs ) == ResolveCharacter( rhs );
 			}
 
-			USE_RESULT static constexpr bool lt(const Char_t& lhs, const Char_t& rhs) noexcept
+			USE_RESULT static constexpr bool lt( const Char_t& lhs, const Char_t& rhs ) noexcept
 			{
-				return ResolveCharacter(lhs) < ResolveCharacter(rhs);
+				return ResolveCharacter( lhs ) < ResolveCharacter( rhs );
 			}
 
 		private:
-			static constexpr Char_t ResolveCharacter(const Char_t ch)
+			static constexpr Char_t ResolveCharacter( const Char_t ch )
 			{
-				if (ch == static_cast<Char_t>('\\'))
-					return static_cast<Char_t>('/');
+				if ( ch == static_cast<Char_t>( '\\' ) )
+					return static_cast<Char_t>( '/' );
 
 				return ch;
-
 			}
 		};
 
-		template<typename CharT>
+		template <typename CharT>
 		struct DOSPathTraits : public std::char_traits<CharT>
 		{
 		public:
@@ -75,11 +80,15 @@ namespace klib
 			using Char_t = CharT;
 			using Base_t = std::char_traits<Char_t>;
 
-			USE_RESULT static constexpr const char* find(const Char_t* haystack, size_t count,
-				const Char_t& needle) noexcept
+			USE_RESULT static constexpr const char* find(
+				const Char_t* haystack, size_t count,
+				const Char_t& needle
+			) noexcept
 			{
-				for (; 0 < count; --count, ++haystack) {
-					if (ResolveCharacter(*haystack) == ResolveCharacter(needle)) {
+				for ( ; 0 < count; --count, ++haystack )
+				{
+					if ( ResolveCharacter( *haystack ) == ResolveCharacter( needle ) )
+					{
 						return haystack;
 					}
 				}
@@ -87,14 +96,16 @@ namespace klib
 				return nullptr;
 			}
 
-			USE_RESULT static constexpr int compare(const Char_t* strA,
-				const Char_t* strB, size_t count) noexcept
+			USE_RESULT static constexpr int compare(
+				const Char_t* strA,
+				const Char_t* strB, size_t count
+			) noexcept
 			{
-				for (; 0 < count; --count, ++strA, ++strB)
+				for ( ; 0 < count; --count, ++strA, ++strB )
 				{
-					const auto letterA = ResolveCharacter(*strA);
-					const auto letterB = ResolveCharacter(*strB);
-					if (letterA != letterB)
+					const auto letterA = ResolveCharacter( *strA );
+					const auto letterB = ResolveCharacter( *strB );
+					if ( letterA != letterB )
 					{
 						return letterA < letterB ? -1 : 1;
 					}
@@ -102,32 +113,32 @@ namespace klib
 				return 0;
 			}
 
-			USE_RESULT static constexpr bool eq(const Char_t& lhs, const Char_t& rhs) noexcept
+			USE_RESULT static constexpr bool eq( const Char_t& lhs, const Char_t& rhs ) noexcept
 			{
-				return ResolveCharacter(lhs) == ResolveCharacter(rhs);
+				return ResolveCharacter( lhs ) == ResolveCharacter( rhs );
 			}
 
-			USE_RESULT static constexpr bool lt(const Char_t& lhs, const Char_t& rhs) noexcept
+			USE_RESULT static constexpr bool lt( const Char_t& lhs, const Char_t& rhs ) noexcept
 			{
-				return ResolveCharacter(lhs) < ResolveCharacter(rhs);
+				return ResolveCharacter( lhs ) < ResolveCharacter( rhs );
 			}
 
 		private:
-			static constexpr Char_t ResolveCharacter(const Char_t ch)
+			static constexpr Char_t ResolveCharacter( const Char_t ch )
 			{
-				if (ch == static_cast<Char_t>('\\'))
-					return static_cast<Char_t>('/');
+				if ( ch == static_cast<Char_t>( '\\' ) )
+					return static_cast<Char_t>( '/' );
 
-				return kString::ToUpper(ch);
+				return kString::ToUpper( ch );
 			}
 		};
 
 		namespace secret::impl
 		{
-			constexpr auto s_IsDos = (kPlatform::GetPlatform() & kPlatform::PlatformOS::WINDOWS) != 0;
+			constexpr auto s_IsDos = ( kPlatform::GetPlatform() & kPlatform::PlatformOS::WINDOWS ) != 0;
 		}
 
-		template<typename CharT>
+		template <typename CharT>
 		using PathTraits = std::conditional_t<secret::impl::s_IsDos, DOSPathTraits<CharT>, UnixPathTraits<CharT>>;
 	}
 }
