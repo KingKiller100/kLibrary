ff6e51fc3cf1a4f41c0300060323388b1c9adcd5
King Kay Dee <kwekudeheer@ymail.com>
2021-11-23 00:13:17 +0000

    Adding log profile type

---
+++ Source/Utility/Logging/kLogProfile.cpp
@@ -0,0 +1,15 @@
+﻿#include "pch.hpp"
+
+#include "kLogProfile.hpp"
+
+namespace klib::kLogs
+{
+	LogProfile::LogProfile( const std::string_view& profileName )
+		: name( profileName )
+	{ }
+
+	std::string_view LogProfile::GetName() const noexcept
+	{
+		return name;
+	}
+}
---
+++ Source/Utility/Logging/kLogProfile.hpp
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+#include <string>
+
+namespace klib::kLogs
+{
+	class LogProfile
+	{
+	public:
+		LogProfile( const std::string_view& profileName );
+
+		[[nodiscard]] std::string_view GetName() const noexcept;
+
+	private:
+		std::string name;
+	};
+}
