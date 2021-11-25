7219f432d391a4bc62f709b3ea55b6ada63d6215
King Kay Dee <kwekudeheer@ymail.com>
2021-11-25 01:39:56 +0000

    Need to redesign regression and unit tests

--- Tests/Utility Tests/Logging_Test.cpp
+++ Tests/Utility Tests/Logging_Test.cpp
@@ -145,12 +145,12 @@ namespace kTest::utility
 		auto testLogger = std::make_unique<Logging>(path);
 
 		testLogger->GetFile().SetFormat("[&N] [&p]: &t", LogLevel::BNR);
-		testLogger->AddBanner("Intro", "Welcome to logging test", "*", "*", 20);
+		testLogger->AddBanner("Welcome to logging test", "*", "*", 20 );
 		testLogger->GetFile().Close(false);
 
 		{
-			testLogger->AddBanner("TEST", "BANNER!",
-				"*", "*", 12);
+			testLogger->AddBanner("BANNER!", "*",
+				"*", 12 );
 			const auto& last = testLogger->GetLastCachedEntry();
 			VERIFY(last.HasText("BANNER!"));
 			VERIFY(last.HasDescription("TEST"));
@@ -159,7 +159,7 @@ namespace kTest::utility
 		testLogger->SetGlobalLevel(LogLevel::NRM);
 
 		{
-			testLogger->AddEntry( , LogLevel::DBG, "DEBUG!" );
+			testLogger->AddEntry( LogLevel::DBG, , "DEBUG!" );
 			const auto& last = testLogger->GetLastCachedEntry();
 			VERIFY(!last.HasText("DEBUG!"));
 			VERIFY(!last.HasDescription(LogLevel::DBG));
@@ -170,7 +170,7 @@ namespace kTest::utility
 		{
 			constexpr char msg[] = "DEBUG!";
 			constexpr auto desc = LogLevel::DBG;
-			testLogger->AddEntry( , desc, msg );
+			testLogger->AddEntry( desc, , msg );
 			const auto& last = testLogger->GetLastCachedEntry();
 			VERIFY(last.HasText(msg));
 			VERIFY(last.HasDescription(desc));
@@ -179,7 +179,7 @@ namespace kTest::utility
 		{
 			constexpr char msg[] = "NORMAL!";
 			constexpr auto desc = LogLevel::NRM;
-			testLogger->AddEntry( , desc, msg );
+			testLogger->AddEntry( desc, , msg );
 			const auto hasCache = testLogger->HasCache();
 			VERIFY(hasCache);
 			const auto& last = testLogger->GetLastCachedEntry();
@@ -190,7 +190,7 @@ namespace kTest::utility
 		{
 			constexpr char msg[] = "INFORMATIVE!";
 			constexpr auto desc = LogLevel::INF;
-			testLogger->AddEntry( , desc, msg );
+			testLogger->AddEntry( desc, , msg );
 			const auto& last = testLogger->GetLastCachedEntry();
 			VERIFY(last.HasText(msg));
 			VERIFY(last.HasDescription(desc));
@@ -199,7 +199,7 @@ namespace kTest::utility
 		{
 			constexpr char msg[] = "WARNING!";
 			constexpr auto desc = LogLevel::WRN;
-			testLogger->AddEntry( , desc, msg );
+			testLogger->AddEntry( desc, , msg );
 			const auto& last = testLogger->GetLastCachedEntry();
 			VERIFY(last.HasText(msg));
 			VERIFY(last.HasDescription(desc));
@@ -208,7 +208,7 @@ namespace kTest::utility
 		{
 			constexpr char msg[] = "ERROR!";
 			constexpr auto desc = LogLevel::ERR;
-			testLogger->AddEntry( , desc, msg );
+			testLogger->AddEntry( desc, , msg );
 			const auto& last = testLogger->GetLastCachedEntry();
 			VERIFY(last.HasText(msg));
 			VERIFY(last.HasDescription(desc));
@@ -224,7 +224,7 @@ namespace kTest::utility
 		{
 			constexpr char msg[] = "ERROR AGAIN!";
 			constexpr auto desc = LogLevel::ERR;
-			testLogger->AddEntry( , desc, msg );
+			testLogger->AddEntry( desc, , msg );
 			const auto& last = testLogger->GetLastCachedEntry();
 			VERIFY(last.HasText(msg));
 			VERIFY(last.HasDescription(desc));
@@ -240,7 +240,7 @@ namespace kTest::utility
 		{
 			constexpr char msg[] = "END!";
 			constexpr auto desc = LogLevel::NRM;
-			testLogger->AddEntry( , desc, msg );
+			testLogger->AddEntry( desc, , msg );
 			const auto hasCache = testLogger->HasCache();
 			VERIFY(!hasCache);
 
@@ -272,7 +272,7 @@ namespace kTest::utility
 		auto& dest = dummy.GetExtraDestination<DummyLogger>(0);
 		dest.Open();
 		dest.SetFormat("&n: [&p]: &t", LogLevel::NRM);
-		dummy.AddEntry( , LogLevel::NRM, "Normal test" );
+		dummy.AddEntry( LogLevel::NRM, , "Normal test" );
 		dummy.Flush();
 
 		VERIFY(dest.IsOpen());
