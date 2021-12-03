b4fb6afd0d201bfdcb209504077175c7042db3ce
King Kay Dee <kwekudeheer@ymail.com>
2021-12-03 02:14:06 +0000

    Log library specific exceptions

---
+++ Source/Utility/Debug/Exceptions/LoggingExceptions.cpp
@@ -0,0 +1,6 @@
+﻿#include "pch.hpp"
+
+#include "LoggingExceptions.hpp"
+
+namespace klib::kDebug
+{ }
---
+++ Source/Utility/Debug/Exceptions/LoggingExceptions.hpp
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+#include "ExceptionBase.hpp"
+
+namespace klib::kDebug
+{
+	class LoggingExceptions : ExceptionBase
+	{
+		LoggingExceptions( std::string_view message )
+			: ExceptionBase( message )
+		{ }
+	};
+
+	class LogProfileError
+	{
+	public:
+		LogProfileError();
+	};
+}
