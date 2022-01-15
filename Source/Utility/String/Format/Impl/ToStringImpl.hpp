747632ce62dcf6e393193c8481915665bcba20db
King Kay Dee <kwekudeheer@ymail.com>
2022-01-15 20:39:05 +0000

    Reformatings file

--- Source/Utility/String/Format/Impl/ToStringImpl.hpp
+++ Source/Utility/String/Format/Impl/ToStringImpl.hpp
@@ -10,46 +10,52 @@
 
 namespace klib::kString::secret::impl
 {
-	template<class Char_t>
-	USE_RESULT constexpr size_t FindOpenerPosition(const std::basic_string<Char_t>& outFmt, const size_t argIndex, const size_t offset)
+	template <class Char_t>
+	USE_RESULT constexpr size_t FindOpenerPosition( const std::basic_string<Char_t>& outFmt, const size_t argIndex, const size_t offset )
 	{
 		using namespace stringify;
 		using Str_t = std::basic_string<Char_t>;
 		constexpr size_t npos = type_trait::g_NoPos<Str_t>;
 
 		const auto data = outFmt.data();
-		
-		Char_t buff[5]{ Char_t(), Char_t(), Char_t(), format::g_ToStringCloserSymbol<Char_t> }; // '{' 'x' 'x' '}/:' '\0'
-		Char_t* const pCloser = std::end(buff) - 2;
+
+		Char_t buff[5]{Char_t(), Char_t(), Char_t(), format::g_ToStringCloserSymbol<Char_t>}; // '{' 'x' 'x' '}/:' '\0'
+		Char_t* const pCloser = std::end( buff ) - 2;
 		Char_t* searchStr = pCloser;
 
-		searchStr = stringify::UintToStr(searchStr, argIndex, 10);
-		*(--searchStr) = format::g_ToStringOpenerSymbol<Char_t>;
+		searchStr = stringify::UintToStr( searchStr, argIndex, 10 );
+		*( --searchStr ) = format::g_ToStringOpenerSymbol<Char_t>;
 
-		size_t openerPos = Find(data, searchStr, offset);
-		if (openerPos != npos)
+		size_t openerPos = Find( data, searchStr, offset );
+		if ( openerPos != npos )
 			return openerPos;
 
-		openerPos = Find(data, searchStr, 0);
-		if (openerPos != npos)
+		openerPos = Find( data, searchStr, 0 );
+		if ( openerPos != npos )
 			return openerPos;
 
 		*pCloser = format::g_ToStringSpecifierSymbol<Char_t>;
-		openerPos = Find(data, searchStr, offset);
+		openerPos = Find( data, searchStr, offset );
 
-		if (openerPos != npos)
+		if ( openerPos != npos )
 			return openerPos;
 
-		openerPos = Find(data, searchStr, 0);
+		openerPos = Find( data, searchStr, 0 );
 		return openerPos;
 	}
 
-	template<class Char_t>
-	constexpr void ToStringImpl(const std::basic_string<Char_t>&, const size_t, const size_t)
+	template <class Char_t>
+	constexpr void ToStringImpl( const std::basic_string<Char_t>&, const size_t, const size_t )
 	{}
 
-	template<class Char_t, typename T, typename ...Ts>
-	constexpr void ToStringImpl(std::basic_string<Char_t>& outFmt, const size_t argIndex, const size_t offset, type_trait::SizeCondConstRef_t<T> arg, type_trait::SizeCondConstRef_t<Ts> ...argPack)
+	template <class Char_t, typename T, typename ...Ts>
+	constexpr void ToStringImpl(
+		std::basic_string<Char_t>& outFmt
+		, const size_t argIndex
+		, const size_t placeholderPos
+		, type_trait::SizeCondConstRef_t<T> arg
+		, type_trait::SizeCondConstRef_t<Ts> ...argPack
+	)
 	{
 		using namespace stringify;
 		using Str_t = std::basic_string<Char_t>;
@@ -57,47 +63,47 @@ namespace klib::kString::secret::impl
 
 		try
 		{
-			size_t openerPos = FindOpenerPosition(outFmt, argIndex, offset);
-			size_t closerPos = Find_First_Of(GetData(outFmt), format::g_ToStringCloserSymbol<Char_t>, openerPos);
+			size_t openerPos = FindOpenerPosition( outFmt, argIndex, placeholderPos );
+			size_t closerPos = Find_First_Of( GetData( outFmt ), format::g_ToStringCloserSymbol<Char_t>, openerPos );
 
 			const auto initialOpenerPos = openerPos;
 			size_t repSize = 0;
 
-			while (openerPos != npos && closerPos != npos)
+			while ( openerPos != npos && closerPos != npos )
 			{
 				const auto infoSize = closerPos - openerPos;
-				const size_t colonPos = Find_First_Of(GetData(outFmt), format::g_ToStringSpecifierSymbol<Char_t>, openerPos);
+				const size_t colonPos = Find_First_Of( GetData( outFmt ), format::g_ToStringSpecifierSymbol<Char_t>, openerPos );
 
 				StringWriter<Char_t> specifier;
 
-				if (openerPos < colonPos
-					&& closerPos > colonPos)
+				if ( openerPos < colonPos
+					&& closerPos > colonPos )
 				{
 					const auto startPos = colonPos + 1;
 					const auto count = closerPos - startPos;
-					specifier = outFmt.substr(startPos, count);
+					specifier = outFmt.substr( startPos, count );
 				}
 
-				const auto replacement = Identity<Char_t, T>::MakeStr(arg, specifier);
-				repSize = GetSize(replacement);
-				outFmt.erase(openerPos, infoSize + 1);
-				outFmt.insert(openerPos, GetData(replacement), repSize);
+				const auto replacement = Identity<Char_t, T>::MakeStr( arg, specifier );
+				repSize = GetSize( replacement );
+				outFmt.erase( openerPos, infoSize + 1 );
+				outFmt.insert( openerPos, GetData( replacement ), repSize );
 
-				if constexpr(type_trait::Is_CString_V<decltype(replacement)>)
+				if constexpr ( type_trait::Is_CString_V<decltype(replacement)> )
 				{
 					delete[] replacement;
 				}
 
-				openerPos = FindOpenerPosition(outFmt, argIndex, openerPos + repSize);
-				if (openerPos != npos)
-					closerPos = Find_First_Of(GetData(outFmt), format::g_ToStringCloserSymbol<Char_t>, openerPos);
+				openerPos = FindOpenerPosition( outFmt, argIndex, openerPos + repSize );
+				if ( openerPos != npos )
+					closerPos = Find_First_Of( GetData( outFmt ), format::g_ToStringCloserSymbol<Char_t>, openerPos );
 			}
 
-			ToStringImpl<Char_t, Ts...>(outFmt, argIndex + 1, initialOpenerPos + repSize, argPack...);
+			ToStringImpl<Char_t, Ts...>( outFmt, argIndex + 1, initialOpenerPos + repSize, argPack... );
 		}
-		catch (const std::exception& e)
+		catch ( const std::exception& e )
 		{
-			throw kDebug::FormatError(e.what());
+			throw kDebug::FormatError( e.what() );
 		}
 	}
 }
