4bc84c6108f2e3ba8bf31d72a5647584b12898d6
King Kay Dee <kwekudeheer@ymail.com>
2022-02-21 01:15:25 +0000

    Introducing scoped thread
    Uses a thread in a scope that will automatically attempt to join main thread upon end of lifetime (can set on exit func to prep thread for terimation)

---
+++ Source/Utility/Thread/ScopedThread.cpp
@@ -0,0 +1,42 @@
+﻿#include "pch.hpp"
+#include "ScopedThread.hpp"
+
+namespace klib::kThread
+{
+	ScopedThread::ScopedThread() = default;
+
+	ScopedThread::ScopedThread( std::thread t, std::function<void()> exitEvent )
+		: thread_( std::move( t ) )
+		, onExitEvent_( std::move( exitEvent ) )
+	{}
+
+	ScopedThread::~ScopedThread()
+	{
+		if ( thread_.joinable() )
+		{
+			if ( onExitEvent_ )
+				onExitEvent_();
+			thread_.join();
+		}
+	}
+
+	bool ScopedThread::IsActive() const noexcept
+	{
+		return thread_.joinable();
+	}
+
+	void ScopedThread::SetThread( std::thread&& t )
+	{
+		thread_ = std::move( t );
+	}
+
+	void ScopedThread::SetExitEvent( std::function<void()> exitEvent )
+	{
+		onExitEvent_ = std::move( exitEvent );
+	}
+
+	auto ScopedThread::GetID() const
+	{
+		return thread_.get_id();
+	}
+}
---
+++ Source/Utility/Thread/ScopedThread.hpp
@@ -0,0 +1,28 @@
+﻿#pragma once
+#include <functional>
+#include <thread>
+
+namespace klib::kThread
+{
+	class ScopedThread
+	{
+	public:
+		explicit ScopedThread();
+
+		explicit ScopedThread( std::thread t, std::function<void()> exitEvent );
+
+		~ScopedThread();
+
+		[[nodiscard]] bool IsActive() const noexcept;
+
+		void SetThread( std::thread&& t );
+
+		void SetExitEvent( std::function<void()> exitEvent );
+
+		[[nodiscard]] auto GetID() const;
+
+	private:
+		std::thread thread_;
+		std::function<void()> onExitEvent_;
+	};
+}
